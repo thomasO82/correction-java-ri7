8c379eae3cf977bab1453c922355e29bfc01be48
thomasO82 <thomas.oliver@ri7.fr>
2025-04-07 11:32:33 +0200

    correction attack

---
+++ src/main/java/fr/sombre_lutin/Main.java
@@ -0,0 +1,11 @@
+// Déclaration du package pour organiser notre code
+package fr.sombre_lutin;
+
+// Définition de la classe principale du programme
+public class Main {
+
+    // Méthode principale : point d'entrée du programme
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/fr/sombre_lutin/model/Attack.java
@@ -0,0 +1,50 @@
+package fr.sombre_lutin.model;
+import java.util.Random; //importation de la classe Random pour générer des nombres aléatoires
+
+//declaration de la classe Person
+public class Attack {
+    String name;
+    int power;
+    int probability;
+    //constructeur de la classe
+    public Attack(String name, int power, int probability) {
+        this.name = name;
+        this.power = power;
+        this.probability = probability;
+    }
+
+    //getters et setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getProbability() {
+        return probability;
+    }
+
+    public void setProbability(int probability) {
+        this.probability = probability;
+    }
+
+    //méthode pour vérifier si l'attaque a réussi
+    public boolean isSuccess() {
+        // Crée une instance de la classe Random
+        Random random = new Random();
+        // Génère un nombre aléatoire entre 0 et 99 et le compare à la probabilité de l'attaque
+        return random.nextInt(100) < this.probability;
+    }
+
+}
