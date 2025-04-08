7f43503bedf96d8d52f3794ec999c6ad0fdef099
thomasO82 <thomas.oliver@ri7.fr>
2025-04-08 14:57:31 +0200

    add character class

--- src/main/java/fr/sombre_lutin/model/Character.java
+++ src/main/java/fr/sombre_lutin/model/Character.java
@@ -1,9 +1,10 @@
 package fr.sombre_lutin.model;
 
 public class Character {
+    // Attributs de la classe Character
     private String name;
     private int life;
-    private Attack[] attacks;
+    private Attack[] attacks;// tableau d'attaques, on utilise la classe Attack pour définir le type d'attaques
 
     public Character(String name, int life, Attack[] attacks) {
         this.name = name;
@@ -29,7 +30,10 @@ public class Character {
     public void setAttacks(Attack[] attacks) {
         this.attacks = attacks;
     }
+
+    //Méthode pour attaquer un personnage
     public void attack(Attack atk, Character car, Character opponent){
+        // pour la concatenation de chaines de caractères, on utilise le + commme en javascript
         System.out.println(car.name + " a choisi " + atk.getName());
         if (atk.isSuccess()){
             if (atk.getPower() < 0){
