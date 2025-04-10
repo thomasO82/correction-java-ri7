2c251d853674a3a50c6c31df117f600bdf213ebb
thomasO82 <thomas.oliver@ri7.fr>
2025-04-10 14:22:00 +0200

    call method game in class main

--- src/main/java/fr/sombre_lutin/Main.java
+++ src/main/java/fr/sombre_lutin/Main.java
@@ -1,11 +1,13 @@
 // Déclaration du package pour organiser notre code
 package fr.sombre_lutin;
 
+import fr.sombre_lutin.controller.Game;
+
 // Définition de la classe principale du programme
 public class Main {
 
     // Méthode principale : point d'entrée du programme
     public static void main(String[] args) {
-
+        Game.game(); // Appel de la méthode game() de la classe Game pour démarrer le jeu
     }
 }
